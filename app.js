d77ad72e7d2911326e0d68e474199c7cf23f6a92
Abel <abel@qburst.com>
2018-06-08 14:00:01 +0530

    Add new port config for heroku

--- app.js
+++ app.js
@@ -9,3 +9,8 @@ app.get('/',(req,res)=>{
 })
 
 //app.listen(3000,()=>console.log("List on port 3000"))
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
