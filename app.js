aca569b6f955bbe41a5b159643f926019572c94b
Abel <abel@qburst.com>
2018-06-08 12:37:30 +0530

    Added Express Framework and Initial Circle CI config

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const public = path.join(__dirname,'public')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(public,'index.html'))
+})
+
+app.listen(3000,()=>console.log("List on port 3000"))
